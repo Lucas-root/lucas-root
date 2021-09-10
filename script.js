145d409d56a00db0fda20065cb71543746ea9f16
lucas <lucasedux0@gmail.com>
2021-09-10 17:59:14 -0300

    Criei o script que configura o width do usuario

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ var estilo = document.getElementById(ele.innerText);
 }
 estilo.style.color = '#3C8C03';
 
+var height_header = document.getElementsByTagName('header')[0].offsetHeight;
+var container_content = document.getElementsByClassName('container-content')[0];
+container_content.style.marginTop = height_header+'px';
+
+var height_container_content = container_content.style.offsetHeight;
+var body = document.getElementsByTagName('body')[0];
+body.style.minHeight = 'calc(101vh - '+ height_header + 'px - ' + height_container_content +'px';
