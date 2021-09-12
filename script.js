d3c3c16c7c45e6d07072f09579ea0df6ae70e59e
lucas <lucasedux0@gmail.com>
2021-09-12 00:56:50 -0300

    Deixei a loja responsiva: agora em divices menores os 2 primeiros produtos quebram; deixei o header responsivo: agora existe um menu de hamburguer no qual quando clicado abre na tela do usuário e com animação

--- script.js
+++ script.js
@@ -9,15 +9,44 @@ var estilo = document.getElementById(ele.innerText);
 estilo.style.color = '#3C8C03';
 
 var height_header = document.getElementsByTagName('header')[0].offsetHeight;
-var container_content = document.getElementById('todo-conteudo');
-container_content.style.marginTop = height_header+'px';
+var todo_conteudo = document.getElementById('todo-conteudo');
+todo_conteudo.style.marginTop = height_header+'px';
+console.log(height_header)
 
-var height_container_content = container_content.style.offsetHeight;
+var height_todo_conteudo = todo_conteudo.style.offsetHeight;
 var body = document.getElementsByTagName('body')[0];
-body.style.minHeight = 'calc(101vh - '+ height_header + 'px - ' + height_container_content +'px';
+body.style.minHeight = 'calc(101vh - '+ height_header + 'px - ' + height_todo_conteudo +'px';
 
 
+// calculo margin para o footer
 var foo = document.getElementsByTagName('footer')[0]
 var wi = foo.offsetHeight;
 var bo = document.getElementsByTagName('body')[0];
 bo.style.paddingBottom = wi + 'px';
+
+
+// estratégia para responsividade do menu
+
+
+let show = true;
+const menuSection = document.querySelector(".toggle-menu");
+const menuToggle = menuSection.querySelector(".menu-toggle");
+var logoLink = document.querySelector('#container-logo');
+var displayLogo = logoLink.style.display;
+
+menuToggle.addEventListener("click", () => {
+
+	//funcionalidade do botão
+	menuSection.classList.toggle("on", show);
+	show = !show;
+
+	// remover a logo ao clicar no botão
+	if (displayLogo != 'none'){
+		logoLink.style.display = 'none';
+		displayLogo = 'none';
+		
+	}else {
+		logoLink.style.display = 'flex';
+		displayLogo = 'flex';
+	}
+})
